79afb0090d0a73f1de493f364e2258eea6e99fa7
fjribgs <fajribagaskara@upi.edu>
2025-06-09 21:05:44 +0700

    Menambahkan fungsi untuk deklarasi ruangan dalam tree

--- main.cpp
+++ main.cpp
@@ -2,3 +2,47 @@
 #include <string>
 using namespace std;
 
+struct treeRuangan
+{
+    string namaRuangan;
+    string deskripsi;
+    bool adaMonster;
+    bool adaHarta;
+    treeRuangan *kiri, *kanan;
+};
+
+treeRuangan *pohonRuangan, *rootRuangan;
+
+void deklarasi()
+{
+    pohonRuangan = NULL;
+}
+
+void insertTree(treeRuangan **rootRuangan, string nama, string deskripsi, bool monster, bool harta)
+{
+    if (*rootRuangan == NULL)
+    {
+        treeRuangan* nodeBaru = new treeRuangan;
+        nodeBaru -> namaRuangan = nama;
+        nodeBaru -> deskripsi   = deskripsi;
+        nodeBaru -> adaMonster  = monster;
+        nodeBaru -> adaHarta    = harta;
+        nodeBaru -> kiri = nodeBaru -> kanan = NULL;
+        *rootRuangan = nodeBaru;
+        return;
+    }
+
+    if (nama.length() < (*rootRuangan) -> namaRuangan.length())
+    {
+        insertTree(&(*rootRuangan) -> kiri, nama, deskripsi, monster, harta);
+    }
+    else
+    {
+        insertTree(&(*rootRuangan) -> kanan, nama, deskripsi, monster, harta);
+    }
+}
+
+int main()
+{
+    
+}
