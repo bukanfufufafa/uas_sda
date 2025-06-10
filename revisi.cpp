a039fcfedbcbe80bf0a8037063176b5f81ab706e
bukanfufufafa <nabilrizky249@gmail.com>
2025-06-10 22:38:12 +0700

    sistem antrian giliran

--- revisi.cpp
+++ revisi.cpp
@@ -29,3 +29,76 @@ struct Karakter
 };
 
 const int maksAntrian=10;
+
+struct antriGiliran
+{
+    Karakter* data[maksAntrian];
+    int depan = 0;
+    
+    bool isEmpty()
+    {
+        if (depan == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    bool isFull()
+    {
+        if (depan == maksAntrian)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void masukAntrian(Karakter* karakter)
+    {
+        if (isFull())
+        {
+            cout << "Antrian Giliran sudah penuh!" << endl;
+        }
+        else
+        {
+            data[depan++] = karakter;
+            cout << "Karakter " << karakter -> namaKarakter << " sudah masuk dalam antrian" << endl;
+        }
+    }
+
+
+    Karakter* keluarAntrian()
+    {
+        if (isEmpty())
+        {
+            cout << "Antrian Giliran Kosong!" << endl;
+            return NULL;
+        }
+        else
+        {
+            Karakter* barisanDepan = data[0];
+            for (int i = 1; i < depan; i++)
+            {
+                data[i - 1] = data[i];
+            }
+            depan--;
+            return barisanDepan;
+        }
+    }
+};
+
+void urutanGiliran(Karakter* arr[], int jumlah)
+{
+    for (int i = 0; i < jumlah - 1; i++)
+    {
+        for (int j = 0; j < jumlah - i - 1; j++)
+        {
+            if (arr[j] -> speed < arr[j + 1] -> speed)
+            {
+                Karakter* temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
